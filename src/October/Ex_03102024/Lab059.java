e64ba07a994d3b30e9e926d9bed80b2437bbe799
Manjunath Gaddi <gaddimanju2@gmail.com>
2024-10-29 21:57:34 +0530

    Task 29th Oct 2024 | Array

--- src/October/Ex_03102024/Lab059.java
+++ src/October/Ex_03102024/Lab059.java
@@ -2,7 +2,7 @@ package October.Ex_03102024;
 
 public class Lab059 {
     public static void main(String[] args) {
-        int [] arr={1,2,3,4,5,5,6,7,7,8,9,10};
+        int [] arr={1,2,3,4,5,6,7,8,9,10};
         int result=duplicates(arr);
         System.out.println("The duplicate element is "+result);
     }
@@ -14,6 +14,6 @@ public class Lab059 {
                 }
             }
         }
-        return Integer.MIN_VALUE;
+        return 0;
     }
 }
